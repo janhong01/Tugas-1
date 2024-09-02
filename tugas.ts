f8d8ee15b49224d80a0fb721922c4d58c4ec5f4a
janhong01 <janhong01@outlook.com>
2024-09-02 16:11:09 +0700

    Tugas Module 1 Logic

---
+++ tugas.ts
@@ -0,0 +1,25 @@
+let _length : number = 5;
+let _width : number  = 3;
+console.log(_length * _width);
+console.log(2*(_length + _width));
+
+let _radius : number = 5;
+let _phi : number = 22/7;
+console.log(2 * _radius);
+console.log((2*_phi*_radius).toFixed(3));
+console.log((_phi*(_radius**2)).toFixed(3))
+
+let a : number = 80;
+let b : number = 65;
+console.log(180-a-b);
+
+// Not finish
+let _date1 : Date = new Date ();
+let day1 = 400;
+let day2 = 366;
+console.log(day1/365)
+
+let date1 : Date = new Date("2022-01-20");
+let date2 : Date = new Date("2022-01-20");
+
+console.log(date2 - date1) 
