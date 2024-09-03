6f55ad58ce8f5d53066d5e4d588a70c69190500a
janhong01 <janhong01@outlook.com>
2024-09-03 20:11:01 +0700

    pembahasan tugas 1

--- tugas.ts
+++ tugas.ts
@@ -4,22 +4,26 @@ console.log(_length * _width);
 console.log(2*(_length + _width));
 
 let _radius : number = 5;
-let _phi : number = 22/7;
+let _phi : number = Math.PI;
 console.log(2 * _radius);
 console.log((2*_phi*_radius).toFixed(3));
 console.log((_phi*(_radius**2)).toFixed(3))
+console.log(_phi* Math.pow(_radius,2))
 
 let a : number = 80;
 let b : number = 65;
 console.log(180-a-b);
 
-// Not finish
+// soal 5
 let _date1 : Date = new Date ();
-let day1 = 400;
+let day1 : number= 366;
 let day2 = 366;
-console.log(day1/365)
+console.log((day1/365).toFixed() +' year, '+
+((day1 % 365)/30).toFixed() +' month, '+
+((day1 % 365)%30).toFixed() +' days');
 
-let date1 : Date = new Date("2022-01-20");
-let date2 : Date = new Date("2022-01-20");
-
-console.log(date2 - date1) 
+//Soal 6
+const date1 : Date = new Date ("2022-01-02");
+const date2 : Date = new Date("2022-01-22");
+const perbedaanDate = (date2.getTime() - date1.getTime())/(24*3600*1000);
+console.log(perbedaanDate);
